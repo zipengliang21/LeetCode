cf068374a2f45f7ca9b40ea66b3922adc6784e2e
Zipeng(Jepson) <zipengliang21@gmail.com>
2020-12-19 21:46:51 -0800

    Learning #1463 from Reference Solution

---
+++ Hard/1463_CherryPickupII.java
@@ -0,0 +1,66 @@
+class Solution {
+    /**
+    * The following code is from the reference solution
+    * Recursion with memorization is using in here
+    * The cherry pick up main function
+    * Initial the storing array
+    *
+    * @param grid - The given 2D matrix
+    */
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dpCache = new int[m][n][n];
+        // initial all elements to -1 to mark unseen
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    dpCache[i][j][k] = -1;
+                }
+            }
+        }
+        return dp(0, 0, n - 1, grid, dpCache);
+    }
+
+    /**
+    * Recursion with memorization helper function
+    * The row and column value have to be in the range
+    * Check the cache of current index of storing array, if it's still -1, 
+    * update with max value that robots can pick up in the future
+    * (Increase row level by one. 3 directions for robot 1 and 3 directions for robot2 -> 9 combinations)
+    *
+    * @param grid - The current row(which is same for both robots)
+    * @param grid - The current column of robot 1
+    * @param grid - The current column of robot 2
+    * @param grid - The given 2D matrix
+    * @param dpCache - The storing Data structure(Our Storing Array)
+    */
+    private int dp(int row, int col1, int col2, int[][] grid, int[][][] dpCache) {
+        if (col1 < 0 || col1 >= grid[0].length || col2 < 0 || col2 >= grid[0].length) {
+            return 0;
+        }
+        // check cache
+        if (dpCache[row][col1][col2] != -1) {
+            return dpCache[row][col1][col2];
+        }
+        // current cell
+        int result = 0;
+        result += grid[row][col1];
+        if (col1 != col2) {
+            result += grid[row][col2];
+        }
+        // transition
+        if (row != grid.length - 1) {
+            int max = 0;
+            for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
+                for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
+                    max = Math.max(max, dp(row + 1, newCol1, newCol2, grid, dpCache));
+                }
+            }
+            result += max;
+        }
+
+        dpCache[row][col1][col2] = result;
+        return result;
+    }
+}
