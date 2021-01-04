02620f610312e247fb577715534aafce14453dfe
Zipeng(Jepson) <zipengliang21@gmail.com>
2021-01-04 11:15:38 -0800

    Complete #21

---
+++ Easy/21_MergeTwoSortedLists.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(-1);
+        ListNode* current = head;
+        while(l1 && l2) {
+            if (l1-> val <= l2-> val) {
+                current->next = l1;
+                l1 = l1->next;
+            } else {
+                current->next = l2;
+                l2 = l2->next;
+            }
+            current = current->next;
+        }
+        current->next = l1? l1 : l2;
+        return head->next;
+    }
+};
---
+++ Easy/21_MergeTwoSortedLists.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (l1, l2) => {
+    if (l1 === null && l2 === null) {
+        return l1;
+    } else if (l1 === null || l2 === null) {
+        return l1 ? l1 : l2;
+    }
+    let head;
+    if (l1.val < l2.val) {
+        head = l1;
+        l1 = l1.next;
+    } else {
+        head = l2;
+        l2 = l2.next;
+    }
+    current = head;
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+    if (l1 !== null) current.next = l1;
+    else if (l2 !== null) current.next = l2;
+    return head;
+};
