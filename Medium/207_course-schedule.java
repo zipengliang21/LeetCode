a5c3ae76cb7d38e337a20ea5c5ebd11a90ad7a5c
Zipeng Liang <zipengliang21@gmail.com>
2022-10-23 21:12:09 -0700

    Complete #207

---
+++ Medium/207_course-schedule.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int index = 0;
+        // [0, 1] => 1 -> 0
+        // construct a adjancency list
+        // key: course number - graphNodes list's index, value: neighbor nodes
+        List<List<Integer>> graphNodes = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graphNodes.add(new ArrayList<>());
+        }
+
+        for (int[] edge : prerequisites) {
+            int start = edge[1];
+            int end = edge[0];
+            graphNodes.get(start).add(end);
+        }
+
+        // calculate the in-degree for all nodes
+        Map<Integer, Integer> indegreeMap = new HashMap<>();
+        for (List<Integer> adjNodes : graphNodes) {
+            for (Integer adjNode : adjNodes) {
+                if (indegreeMap.containsKey(adjNode)) {
+                    indegreeMap.put(adjNode, indegreeMap.get(adjNode) + 1);
+                } else {
+                    indegreeMap.put(adjNode, 1);
+                }
+            }
+        }
+
+        // put all nodes with 0 in-degree into a queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!indegreeMap.containsKey(i)) {
+                queue.offer(i);
+                index++;
+            }
+        }
+
+        // BFS
+        while (!queue.isEmpty()) {
+            Integer curNode = queue.poll();
+            List<Integer> adjNodes = graphNodes.get(curNode);
+            for (Integer adjNode : adjNodes) {
+                // its neightbor indegree decrease by 1
+                int updatedIndegree = indegreeMap.get(adjNode) - 1;
+                indegreeMap.put(adjNode, updatedIndegree);
+
+                // after update, if its indegree is 0, then put it into queue
+                if (updatedIndegree == 0) {
+                    queue.offer(adjNode);
+                    index++;
+                }
+            }
+        }
+
+        return index == numCourses;
+    }
+}
