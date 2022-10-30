8e8cef5a3f2c6728b4c2f00ddc76d2d01c38ecdf
Zipeng Liang <zipengliang21@gmail.com>
2022-10-29 22:18:53 -0700

    Complete #205 and #242

---
+++ Easy/242_valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            counts[t.charAt(i) - 'a']--;
+
+            if (counts[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
