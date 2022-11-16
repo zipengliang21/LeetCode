6482b503c737f3f2a567ed030c1064d7be4d39c7
Zipeng Liang <zipengliang21@gmail.com>
2022-11-15 23:04:43 -0800

    Complete #122 in DP approach

--- Medium/122_best-time-to-buy-and-sell-stock-ii.java
+++ Medium/122_best-time-to-buy-and-sell-stock-ii.java
@@ -1,3 +1,23 @@
+// DP
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        int n = prices.length;
+        int[][] dp = new int[2][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] + prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] - prices[i]);
+        }
+
+        return dp[(n - 1) % 2][0];
+    }
+}
+
+// Greedy algorithm
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
